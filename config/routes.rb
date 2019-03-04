88f8a2ba06c2a2acfcc6b8e6eacdfc5cc586c017
MarionA29 <marionaudren@live.fr>
2019-03-04 15:23:40 +0100

    modif routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'items#index'
+  root 'item#index'
   resources :items
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

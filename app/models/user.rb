d2bbdfe6e4059b0f1d3c02dc48300a8650994862
MarionA29 <marionaudren@live.fr>
2019-03-05 09:52:59 +0100

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
 validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
-validates :password, presence: true 
-has_many :purchases, dependent: :destroy
-has_many :items, through: :purchases
+validates :password, presence: true
+has_one :purchase, dependent: :destroy
+has_many :items, through: :purchase
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

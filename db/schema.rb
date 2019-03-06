2075ac6b2e403548130e88ccaeaf3984cfb6189f
MarionA29 <marionaudren@live.fr>
2019-03-06 17:20:56 +0100

    add user name

--- db/schema.rb
+++ db/schema.rb
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2019_03_05_140626) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true

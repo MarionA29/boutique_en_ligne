327aa5ea0487276a86c68f72c5bdaabe14fedc1c
totaotata <totaotata@protonmail.com>
2019-03-07 17:10:25 +0100

    modif seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'faker'
 User.destroy_all
 Item.destroy_all
-Cart.destroy_all
+
 
 10.times do
   User.create!(first_name: Faker::Name.first_name,last_name: Faker::Name.last_name, email: Faker::Internet.email, password: Faker::Space.agency)
